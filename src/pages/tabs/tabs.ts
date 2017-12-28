a8f311809b928d4c1d7838f856fa9b158f120681
secforgeek <secforgeek@gmail.com>
2017-12-28 00:39:37 +0000

    Show Menu Initial Template and Testing Modification in some pages

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,17 +2,17 @@ import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular'
 import { OrdersPage } from '../orders/orders';
 import { SettingsPage } from '../settings/settings';
-//import { HomePage } from '../home/home';
-import { ListshopsPage } from '../listshops/listshops';
-import { ShowmenuPage } from '../showmenu/showmenu';
+//import { ListshopsPage } from '../listshops/listshops';
+//import { ShowmenuPage } from '../showmenu/showmenu';
+import { HomePage } from '../home/home';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = ShowmenuPage;
-  tab2Root = ListshopsPage;//OrdersPage;
+  tab1Root = HomePage; //ShowmenuPage;
+  tab2Root = OrdersPage;//ListshopsPage;//
   tab3Root = SettingsPage;
   mySelectedIndex: number;
 
